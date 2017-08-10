c7b0b1f8c51f17e5fc624cb792f3599b1d8f1acc
Kiarash Mohammadi <kiarash.km@gmail.com>
2017-08-10 04:27:46 -0400

    hashtags

--- test_hashtags.py
+++ test_hashtags.py
@@ -1,17 +1,21 @@
 import file_reader
 import os
 
-#for f in os.listdir('cleansed'):
-    #x = file_reader.read_csv('cleansed/'+f)
-x = file_reader.read_csv('cleansed/G5--2016-5-27--1.csv')
-for row in x:
-    if len(row)>6 and row[6] != 'NULL':
-        print('__________________________________')
-        s = row[6]
-        print(s)
-        tmp = s.split()
-        for word in tmp:
-            for c in word:
-                if c=='#':
-                    print(word)
+hashtags_occ = {}
+
+for f in os.listdir('cleansed'):
+    x = file_reader.read_csv('cleansed/'+f)
+    for row in x:
+        if len(row)>6 and row[6] != 'NULL':
+            s = row[6]
+            tmp = s.split()
+            for word in tmp:
+                for c in word:
+                    if c=='#':
+                        if word in hashtags_occ:
+                            hashtags_occ[word] += 1
+                        else:
+                            hashtags_occ[word] = 1
+
+
 
