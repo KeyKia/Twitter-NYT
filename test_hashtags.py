dceef51d3dcacbe037e75a79dc37d1d2bb8c396a
Kiarash Mohammadi <kiarash.km@gmail.com>
2017-08-10 12:53:21 +0430

    finding hashtags

--- test_hashtags.py
+++ test_hashtags.py
@@ -3,11 +3,15 @@ import os
 
 #for f in os.listdir('cleansed'):
     #x = file_reader.read_csv('cleansed/'+f)
-    x = file_reader.read_csv('/home/user/Desktop/Twitter-NYT/cleansed/G5--2016-5-27--5.csv')
-    for row in x:
+x = file_reader.read_csv('cleansed/G5--2016-5-27--1.csv')
+for row in x:
+    if len(row)>6 and row[6] != 'NULL':
+        print('__________________________________')
         s = row[6]
-        tmp = s.split(" ")
+        print(s)
+        tmp = s.split()
         for word in tmp:
-            if word[0]=='#' :
-                print(word)
+            for c in word:
+                if c=='#':
+                    print(word)
 
